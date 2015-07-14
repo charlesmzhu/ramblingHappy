48d014f6a0bb2d1a3c14a5fecad39eb081e18674
Charles Zhu <charlesmzhu@gmail.com>
2015-07-13 23:51:10 -0700

    With emails sent out

--- ramblingHappy.js
+++ ramblingHappy.js
@@ -1,23 +1,24 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if(Meteor.isClient) {
+  // on the client, we just have to track the DOM click event on the input.
+  Template.email.events({
+    'click #btn': function () {
+      // if someone click on the button ( tag), then we ask the server to execute the function sendEmail (RPC call)
+      Meteor.call('sendEmail', $('#email').val());
+      Session.set('done', true);
     }
   });
+  Template.email.done = function () { return Session.equals('done', true); }
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+}
+
+if(Meteor.isServer) {
+  // on the server, we create the sendEmail RPC function
+  Meteor.methods({
+    sendEmail: function(email) {
+      // send the email!
+      Email.send({to:email, from: email, subject:'Hello from the future', text: "Think happy bots :)."});
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Statements = new Mongo.Collection ('Statements');
