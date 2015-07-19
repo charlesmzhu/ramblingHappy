f43c9cee56c5dcd5f3e6af00b5797f6ef5ccb948
Charles Zhu <charlesmzhu@gmail.com>
2015-07-19 12:24:49 -0700

    With accounts and router added

--- client/ramblingHappy.js
+++ client/ramblingHappy.js
@@ -1,7 +1,8 @@
 Template.email.events({
   'click #btn': function () {
     // if someone click on the button ( tag), then we ask the server to execute the function sendEmail (RPC call)
-    Meteor.call('sendEmail', $('#email').val());
+    // Meteor.call('sendEmail', $('#email').val() );
+    Meteor.call('addJoy', $('#email').val(), $('#happytext').val() );
     Session.set('done', true);
   }
 });
