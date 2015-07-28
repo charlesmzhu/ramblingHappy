61e943d3ab8bac15b05a88fd22d89e03589683e1
Charles Zhu <charlesmzhu@gmail.com>
2015-07-27 23:12:52 -0700

    Templates created. Need to configure email with 1-month difference.

--- client/ramblingHappy.js
+++ client/ramblingHappy.js
@@ -1,8 +1,22 @@
+Template.joys.helpers ({
+  joys: function () {
+  	return Joys.find();
+  }
+})
+
+Template.joySubmit.events ({
+  'submit #submitJoy': function (e) {
+  	e.preventDefault();
+  	console.log("Submit joy called");
+	Meteor.call('addJoy', $('#joytext').val() );
+	return false;
+  }
+})
+
 Template.email.events({
   'click #btn': function () {
     // if someone click on the button ( tag), then we ask the server to execute the function sendEmail (RPC call)
     // Meteor.call('sendEmail', $('#email').val() );
-    Meteor.call('addJoy', $('#email').val(), $('#happytext').val() );
     Session.set('done', true);
   }
 });
